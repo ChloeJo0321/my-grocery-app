1e085162d11b79a12f602032a5520ea5b937d44c
Chloe Jo <hyeyoonjo321@gmail.com>
2026-07-01 13:54:33 -0300

    feat(app): add page routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,11 @@ import Footer from "/components/Footer";
 import Home from "/pages/Home";
 import ProductDetail from "../pages/productDetail";
 import Cart from "../pages/Cart";
+import SearchResults from "../pages/SearchResults";
+import Account from "../pages/Account";
+import SignIn from "../pages/SignIn";
+import Reorder from "../pages/Reorder";
+import Meat from "../pages/Meat";
 
 function App() {
   return (
@@ -15,7 +20,12 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/freshProduce' element={<FreshProduce />} />
         <Route path='/freshProduce/:id' element={<ProductDetail />}></Route>
+        <Route path='/meat' element={<Meat />}></Route>
         <Route path='/cart' element={<Cart />}></Route>
+        <Route path='/search/' element={<SearchResults />}></Route>
+        <Route path='/account' element={<Account />}></Route>
+        <Route path='/signIn' element={<SignIn />}></Route>
+        <Route path='/reorder' element={<Reorder />}></Route>
       </Routes>
       <Footer />
     </>
