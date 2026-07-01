797ba19a3310d8ea2d25131aedc22f46f86069ee
Chloe Jo <hyeyoonjo321@gmail.com>
2026-07-01 14:03:55 -0300

    feat(cart): add cart GET endpoints

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -2,6 +2,40 @@ const express = require("express");
 const router = express.Router();
 const db = require("../config/db");
 
+// Get and display all items in user's cart
+router.get("/", async (req, res) => {
+  const username = "testUser"; // hardcode -> change it later
+  try {
+    // Get all products data
+    const cart = await db.query("SELECT * FROM cart WHERE username=?", [
+      username,
+    ]);
+    return res.json(cart);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Get product id and check if this is in DB
+router.get("/product", async (req, res) => {
+  const { productId } = req.query;
+  const username = "testUser";
+  const data = await db.query(
+    "SELECT * FROM cart WHERE username=? AND product_id=?",
+    [username, productId],
+  );
+  return res.json(data);
+});
+
+// Get all product records from DB matching the username
+router.get("/user", async (req, res) => {
+  const { username } = req.query;
+  const userData = await db.query("SELECT * FROM cart WHERE username = ?", [
+    username,
+  ]);
+  return res.json(userData);
+});
+
 // Add a new product record to cart DB
 router.post("/", async (req, res) => {
   const {
@@ -28,54 +62,15 @@ router.patch("/", async (req, res) => {
     "UPDATE cart SET product_quantity= ? WHERE username = ? AND product_id = ?",
     [productQuantity, username, productId],
   );
-
-  // Test for update query
-  console.log(query);
 });
 
-// Get and display all items in user's cart
-router.get("/", async (req, res) => {
-  const username = "testUser"; // hardcode -> change it later
-  try {
-    // Get all products data
-    const cart = await db.query("SELECT * FROM cart WHERE username=?", [
-      username,
-    ]);
-    return res.json(cart);
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-// Get all product records from DB matching the username and product id
-router.get("/product", async (req, res) => {
-  const { productId } = req.query;
-  const username = "testUser";
-  const data = await db.query(
-    "SELECT * FROM cart WHERE username=? AND product_id=?",
-    [username, productId],
-  );
-  return res.json(data);
-});
-
-// Get all product records from DB matching the username
-router.get("/user", async (req, res) => {
-  const { username } = req.query;
-  const userData = await db.query("SELECT * FROM cart WHERE username = ?", [
-    username,
-  ]);
-  return res.json(userData);
-});
-
-// Acess and delete a product record from cart DB
+// Access and delete a product record from cart DB
 router.delete("/", async (req, res) => {
   const { productId, username } = req.body;
   const query = await db.query(
     "DELETE from cart WHERE product_id = ? and username = ?",
     [productId, username],
   );
-
-  console.log(query);
 });
 
 module.exports = router;
