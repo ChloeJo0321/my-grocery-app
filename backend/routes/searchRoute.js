34d90b023583212bc9e13e43e21497f4735f97b5
Chloe Jo <hyeyoonjo321@gmail.com>
2026-07-01 14:01:52 -0300

    feat(search): add search API

--- backend/routes/searchRoute.js
+++ backend/routes/searchRoute.js
@@ -2,9 +2,14 @@ const express = require("express");
 const router = express.Router();
 const db = require("../config/db");
 
-router.post("/", async (req, res) => {
-  const { searchWord } = req.body;
-  console.log(searchWord);
+router.get("/", async (req, res) => {
+  const searchWord = req.query.searchWord;
+
+  const query = await db.query("SELECT * FROM cart WHERE product_name LIKE ?", [
+    `${searchWord}%`,
+  ]);
+
+  return res.json(query);
 });
 
 module.exports = router;
